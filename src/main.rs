8b9fe8bf447c910ae2ac671144cf2d2bb7397be8
Vladimir Beliashov <belyash@seznam.cz>
2023-03-15 23:01:43 +0400

    add terminal support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::error::Error;
+use std::{error::Error, io, time::Duration};
+use crossterm::{terminal, ExecutableCommand, cursor, event::{self, Event, KeyCode}};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
+  // audio
   let mut audio = Audio::new();
   audio.add("explode", "explode.wav");
   audio.add("lose", "lose.wav");
@@ -11,7 +13,33 @@ fn main() -> Result<(), Box<dyn Error>> {
   audio.add("win", "win.wav");
 
   audio.play("startup");
+
+  // terminal
+  let mut stdout = io::stdout();
+  terminal::enable_raw_mode()?;
+  stdout.execute(terminal::EnterAlternateScreen)?;
+  stdout.execute(cursor::Hide)?;
   
+  // game loop
+  'gameloop: loop {
+    while event::poll(Duration::default())?  {
+      if let Event::Key(key_event) = event::read()? {
+        match key_event.code {
+            KeyCode::Esc | KeyCode::Char('q') => {
+              audio.play("lose");
+              break 'gameloop;
+            }
+            _ => {}
+        }
+      }
+    }
+  }
+  
+  // cleanup
   audio.wait();
+  stdout.execute(terminal::LeaveAlternateScreen)?;
+  stdout.execute(cursor::Show)?;
+  terminal::disable_raw_mode()?;
+
   Ok(())
 }
