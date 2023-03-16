0112b70658e902ef9ee886d84ba52aec26556bf9
Vladimir Beliashov <belyash@seznam.cz>
2023-03-16 16:28:44 +0400

    pew pew

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, io, time::Duration, sync::mpsc, thread};
+use std::{error::Error, io, time::{Duration, Instant}, sync::mpsc, thread};
 use crossterm::{terminal, ExecutableCommand, cursor, event::{self, Event, KeyCode}};
 use invaders::{frame::{self, Drawable}, render, player::Player};
 use rusty_audio::Audio;
@@ -40,7 +40,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 
   // game loop
   let mut player = Player::new();
+  let mut instant = Instant::now();
   'gameloop: loop {
+    let delta = instant.elapsed();
+    instant = Instant::now();
     let mut curr_frame = frame::new_frame();
 
     while event::poll(Duration::default())?  {
@@ -48,6 +51,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         match key_event.code {
             KeyCode::Left => player.move_left(),
             KeyCode::Right => player.move_right(),
+            KeyCode::Char(' ') | KeyCode::Enter => {
+              if player.shoot() {
+                audio.play("pew");
+              }
+            }
             KeyCode::Esc | KeyCode::Char('q') => {
               audio.play("lose");
               break 'gameloop;
@@ -56,6 +64,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
       }
     }
+
+    player.update(delta);
 
     player.draw(&mut curr_frame);
     let _ = render_tx.send(curr_frame);
